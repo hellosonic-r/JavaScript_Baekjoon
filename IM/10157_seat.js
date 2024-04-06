900f2abc120b96bee3cb828324db9ceb329f8599
hellosonic-r <hellommmh@gmail.com>
2024-04-06 12:50:28 +0900

    S4

--- IM/10157_seat.js
+++ IM/10157_seat.js
@@ -9,42 +9,27 @@ const board = Array.from({ length: m }, () => Array(n).fill(0));
 const dx = [0, 1, 0, -1];
 const dy = [1, 0, -1, 0];
 
-const bfs = (sx, sy) => {
-  const queue = [];
-  queue.push([sx, sy]);
-  board[sy][sx] = 1;
-  let cnt = 1;
-  let turn = 0;
-
-  while (queue.length !== 0) {
-    const [x, y] = queue.shift();
-    let nx = x + dx[turn];
-    let ny = y + dy[turn];
-    if (nx < 0 || nx >= n || ny < 0 || ny >= m || board[ny][nx] !== 0) {
-      nx = x + dx[(turn + 1) % 4];
-      ny = y + dy[(turn + 1) % 4];
-      turn = (turn + 1) % 4;
-      board[ny][nx] = board[y][x] + 1;
-      queue.push([nx, ny]);
-      cnt += 1;
-      if (cnt === k) {
-        console.log(nx + 1, ny + 1);
-        break;
-      }
-    } else {
-      board[ny][nx] = board[y][x] + 1;
-      queue.push([nx, ny]);
-      cnt += 1;
-      if (cnt === k) {
-        console.log(nx + 1, ny + 1);
-        break;
-      }
-    }
-  }
-};
+let x = 0;
+let y = 0;
+let turn = 0;
 
 if (n * m < k) {
   console.log(0);
 } else {
-  bfs(0, 0);
+  for (let i = 1; i < n * m + 1; i += 1) {
+    if (i === k) {
+      console.log(x + 1, y + 1);
+      break;
+    }
+    board[y][x] = i;
+    x += dx[turn];
+    y += dy[turn];
+    if (x < 0 || x >= n || y < 0 || y >= m || board[y][x] !== 0) {
+      x -= dx[turn];
+      y -= dy[turn];
+      turn = (turn + 1) % 4;
+      x += dx[turn];
+      y += dy[turn];
+    }
+  }
 }
