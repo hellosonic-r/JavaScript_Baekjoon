5e4bafbb5a1b254811fe5592dfd9d380dbb569bb
hellosonic-r <hellommmh@gmail.com>
2024-05-18 10:12:12 +0900

    S2

--- IM/2477_melon.js
+++ IM/2477_melon.js
@@ -5,29 +5,18 @@ const [k, ...arr] = input;
 const cnt = Number(k);
 const newArr = arr.map((item) => item.split(' ').map(Number));
 
-let maxWidth = 0;
-let maxHeight = 0;
-let smallWidth = 0;
-let smallHeight = 0;
+let bigSize = 0;
+let bigIdx = 0;
 
-dic = { 1: 0, 2: 0, 3: 0, 4: 0 };
-
-newArr.forEach((tempArr) => (dic[tempArr[0]] += 1));
-
-for (let key in dic) {
-  if (dic[key] === 1) {
-    newArr.forEach((tempArr, idx) => {
-      if (tempArr[0] === Number(key)) {
-        if (maxWidth === 0) {
-          maxWidth = tempArr[1];
-          smallWidth = newArr[(idx + 3) % 6][1];
-        } else {
-          maxHeight = tempArr[1];
-          smallHeight = newArr[(idx + 3) % 6][1];
-        }
-      }
-    });
+for (let i = 0; i < newArr.length; i += 1) {
+  let currentLength = newArr[i][1];
+  let nextLength = newArr[(i + 1) % 6][1];
+  if (bigSize < currentLength * nextLength) {
+    bigSize = currentLength * nextLength;
+    bigIdx = i;
   }
 }
 
-console.log((maxWidth * maxHeight - smallWidth * smallHeight) * k);
+console.log(
+  (bigSize - newArr[(bigIdx + 3) % 6][1] * newArr[(bigIdx + 4) % 6][1]) * cnt
+);
